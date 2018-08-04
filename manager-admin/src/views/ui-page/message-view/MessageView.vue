88f4e7baf4ec887a9fb561f685bbfab2cac3cc39
QianGuoqing <qian_guoqing@yeah.net>
2018-08-04 15:52:19 +0800

    add message-ui和notification-ui

--- manager-admin/src/views/ui-page/message-view/MessageView.vue
+++ manager-admin/src/views/ui-page/message-view/MessageView.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="message-view">
-    message view
+    <a-card title="全局提示框" class="card-wrapper">
+      <a-button @click="success">Success</a-button>
+      <a-button @click="error">Error</a-button>
+      <a-button @click="warning">Warning</a-button>
+    </a-card>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'MessageView'
+    name: 'MessageView',
+    methods: {
+      success () {
+        this.$message.success('This is a message of success');
+      },
+      error () {
+        this.$message.error('This is a message of error');
+      },
+      warning () {
+        this.$message.warning('This is message of warning');
+      },
+    }
   }
 </script>
 
 <style lang="stylus" scoped>
-
+  .message-view
+    margin 10px
+    .card-wrapper
+      margin-bottom 15px
 </style>
--- manager-admin/src/views/ui-page/notification-view/NotificationView.vue
+++ manager-admin/src/views/ui-page/notification-view/NotificationView.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="notification-view">
-    notification view
+    <a-card title="通知提醒框" class="card-wrapper">
+      <a-button @click="() => openNotificationWithIcon('success')">Success</a-button>
+      <a-button @click="() => openNotificationWithIcon('info')">Info</a-button>
+      <a-button @click="() => openNotificationWithIcon('warning')">Warning</a-button>
+      <a-button @click="() => openNotificationWithIcon('error')">Error</a-button>
+    </a-card>
+    <a-card title="通知提醒框" class="card-wrapper">
+      <a-select v-model="selected" :style="{ width: '120px', marginRight: '10px' }">
+        <a-select-option v-for="val in options" :key="val" :value="val">{{val}}</a-select-option>
+      </a-select>
+      <a-button type="primary" @click="openNotification">Open the notification box</a-button>
+    </a-card>
   </div>
 </template>
 
 <script>
+  const options = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight']
   export default {
-    name: 'NotificationView'
+    name: 'NotificationView',
+    data () {
+      return {
+        options,
+        selected: 'topRight',
+      }
+    },
+    watch: {
+      selected(val) {
+        this.$notification.config({
+          placement: val,
+        });
+      }
+    },
+    methods: {
+      openNotificationWithIcon (type) {
+        this.$notification[type]({
+          message: 'Notification Title',
+          description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        });
+      },
+      openNotification (val) {
+        this.$notification.open({
+          message: 'Notification Title',
+          description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        });
+      },
+    }
   }
 </script>
 
 <style lang="stylus" scoped>
-
+  .notification-view
+    margin 10px
+    .card-wrapper
+      margin-bottom 15px
 </style>
