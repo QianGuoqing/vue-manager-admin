c77c14009378dd95ad68e895eb6f185308de56ea
QianGuoqing <qian_guoqing@yeah.net>
2018-08-04 14:44:18 +0800

    添加loading-ui

--- manager-admin/src/views/ui-page/loading-view/LoadingView.vue
+++ manager-admin/src/views/ui-page/loading-view/LoadingView.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="loading-view">
-    loading view
+    <a-card title="加载组件" class="card-wrapper">
+      <a-spin size="small" />
+      <a-spin class="loading-spin" />
+      <a-spin size="large" />
+      <a-spin class="loading-spin">
+        <a-icon slot="indicator" type="loading" style="font-size: 24px" spin />
+      </a-spin>
+    </a-card>
+    <a-card title="内容遮罩" class="card-wrapper">
+      <a-spin>
+        <a-alert message="正在加载的模块" type="info" />
+      </a-spin>
+      <a-spin style="margin-top: 20px" tip="loading">
+        <a-alert message="正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块正在加载的模块" type="info" />
+      </a-spin>
+    </a-card>
   </div>
 </template>
 
@@ -11,5 +26,10 @@
 </script>
 
 <style lang="stylus" scoped>
-
+  .loading-view
+    margin 10px
+    .card-wrapper
+      margin-bottom 15px
+      .loading-spin
+        margin 0 20px
 </style>
